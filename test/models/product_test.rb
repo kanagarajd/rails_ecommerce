d831eea73b240f3a08d2b0e609712971348b19cf
Kanagaraj <kanagaraj@juhomi.com>
2020-08-24 17:25:00 +0530

    add test cases

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -11,7 +11,7 @@ class ProductTest < ActiveSupport::TestCase
    end
 
    test "Price should not be negative" do
-    product = Product.new(title: "Title", description: "description", image_url: "image_url")
+    product = Product.new(title: "Good enough Title New", description: "description", image_url: "image_url")
     assert product.invalid?
     
     product.price = -1
@@ -26,12 +26,23 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product title should be unique" do
-  	product = Product.new(title: "MyString", description: "desc", image_url: "url")
+  	product = Product.new(title: "Good enough MyString New", description: "desc", image_url: "url")
   	assert product.invalid?
 
   	assert product.errors[:title].any?
   	puts product.errors[:title]
 
   	assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  test "product title should be having atleast 10 characters" do
+    product = Product.new(title: "Sony TV", description: "desc", image_url: "url")
+    assert product.invalid?
+    assert product.errors[:title].any?
+    puts product.errors[:title]
+
+    product = Product.new(title: "Good Sony TV", description: "desc", image_url: "url")
+    assert product.valid?
+  
   end
 end
